ff557911f0bccb9ccab3c2962ee2fc895f617ad9
Kaunteya <k.suryawanshi@gmail.com>
2021-05-10 14:51:32 +0530

    Added demo

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '12.0'
+
+target 'YMChatDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+    pod 'YMChat', "~> 1.0.0"
+#    pod 'YMChat', "1.0.0"
+#     pod 'YMChat', :path => '../YMChatbot-iOS'
+  
+end
--- YMChatDemo/ViewController.swift
+++ YMChatDemo/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import YMChat // Note this
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, YMChatDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func presentYM(_ sender: Any) {
+        let config = YMConfig(botId: "x1609740331340")
+        config.statusBarColor = UIColor(red: 81 / 255, green: 133 / 255, blue: 237 / 255, alpha: 1)
+
+        config.enableHistory = true
+        config.payload = ["UserState": "Anonymous"]
+
+        // WARNING: config should be set before invoking startChatBot() method
+        YMChat.shared.config = config
+
+        // Loads the chat window. Pass the current view controller as a parameter
+        YMChat.shared.startChatbot(on: self)
+
+        // Enable logging for debugging purpose
+        YMChat.shared.enableLogging = true
+
+        YMChat.shared.delegate = self
     }
 
-
+    // MARK: - YMChatDelegate
+    func onEventFromBot(response: YMBotEventResponse) {
+        print("Even from a bot has been received", response)
+    }
 }
-
