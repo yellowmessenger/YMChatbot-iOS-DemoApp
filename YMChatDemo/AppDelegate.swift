8fc3c794a03f0797d25892a15925c9f882f9344a
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2021-09-15 11:14:26 +0530

    commented notification setup

--- YMChatDemo/AppDelegate.swift
+++ YMChatDemo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // NOTE: UNCOMMENT FOLLOWING LINE TO ENABLE FIREBASE PUSH NOTIFICATIONS
-         ymNotificationSetup(application)
+//         ymNotificationSetup(application)
 
         return true
     }
